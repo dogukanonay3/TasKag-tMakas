7a16a8a45bae5b0566dce6589cf9fd9529880045
dogukanonay3 <donay3307@gmail.com>
2022-10-26 01:17:48 +0300

    Add project files.

---
+++ TasKagıtMakas/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace TasKagıtMakas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string oyuncu1;
+            string oyuncu2;
+
+            Console.WriteLine("Oyuncu1 secimini giriniz");
+            oyuncu1 = Console.ReadLine();
+            Console.WriteLine("Oyuncu2 secimini giriniz");
+            oyuncu2 = Console.ReadLine();
+
+            if (oyuncu1 == "tas")
+            {
+                if (oyuncu2 == "makas")
+                    Console.WriteLine("oyuncu1 kazandı");
+                if (oyuncu2 == "kagıt")
+                    Console.WriteLine("oyuncu2 kazandı");
+                if (oyuncu2 == "tas")
+                    Console.WriteLine("berabere");
+            }
+            else if (oyuncu1 == "makas")
+            {
+                if (oyuncu2 == "tas")
+                    Console.WriteLine("oyuncu2 kazandı");
+                if (oyuncu2 == "makas")
+                    Console.WriteLine("berabere");
+                if (oyuncu2 == "kagıt")
+                    Console.WriteLine("oyuncu1 kazandı");
+            }
+            else  if (oyuncu1 == "kagıt")
+            {
+                if (oyuncu2 == "tas")
+                    Console.WriteLine("oyuncu1 kazandı");
+                if (oyuncu2 == "makas")
+                    Console.WriteLine("oyuncu2 kazandı");
+                if (oyuncu2 == "kagıt")
+                    Console.WriteLine("berabere");
+            }
+
+        }
+    }
+}
